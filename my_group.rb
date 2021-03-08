386692c4d314841e498eb8865f3b6b3d5963c8bc
sevader14 <vsevolod.deriushkin@gmail.com>
2021-03-08 13:45:46 +0100

    add my_group.rb

---
+++ my_group.rb
@@ -0,0 +1,5 @@
+my_group = []
+person_1 = {name: 'Bob', gender: 'male', age: 21}
+person_2 = {name: 'Nick', gender: 'male', age: 32}
+person_3 = {name: 'Kate', gender: 'female', age: 28}
+my_group = [person_1, person_2, person_3]
