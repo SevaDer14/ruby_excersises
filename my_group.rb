936e6c0f558f3881053ee57c925aa37791f0568d
sevader14 <vsevolod.deriushkin@gmail.com>
2021-03-10 11:27:41 +0100

    all tastks finished

--- my_group.rb
+++ my_group.rb
@@ -3,3 +3,6 @@ person_1 = {name: 'Bob', gender: 'male', age: 21}
 person_2 = {name: 'Nick', gender: 'male', age: 32}
 person_3 = {name: 'Kate', gender: 'female', age: 28}
 my_group = [person_1, person_2, person_3]
+my_group.each do |person|
+    puts "#{person[:name]} is a #{person[:age]} years old #{person[:gender]}"
+end
